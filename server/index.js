40d294415d3f047223843a531e82bf80a93d9a9f
Arvinth-s <arvinth.saravana@gmail.com>
2021-06-27 13:26:39 +0530

    completed get and post request

--- server/index.js
+++ server/index.js
@@ -13,21 +13,67 @@ const db = mysql.createPool({
 
 app.get("/addSubscriber", (req, res)=>{
     let date = new Date();
-    const sqlInsert='insert into subscribers (address, expiry) values ("arvinth", ?)'
-    db.query(sqlInsert, date, function(err, results){
+    const sql='insert into subscribers (address, expiry) values ("arvinth", ?)'
+    db.query(sql, date, function(err, results){
         if(err)console.log('err:', err);
         console.log("results: ", results);
     });
     res.send('inserted');
-})
-
-app.get("/output",(req,res) => {
-    const sqlInsert = "";
-    db.query('select * from subscribers', function(err, results){
-        console.log(results);
-    });
-    res.send("Hello-World");
 });
+
+app.get("/subscribers",(req,res) => {
+    const sql = "select * from subscribers";
+    var data = {
+      Data: "",
+    };
+    db.query(sql, function (err, rows, fields) {
+      if (rows.length != 0) {
+        data["Data"] = rows;
+        res.json(data);
+      } else {
+        data["Data"] = "No data Found..";
+        res.json(data);
+      }
+    });
+});
+
+app.get("/expiry", (req, res) => {
+
+  const sql = "select * from subscribers where address = ?";
+  var data = {
+    Data: "",
+  };
+  db.query(sql, [req.query.address],function (err, rows, fields) {
+    if (rows.length != 0) {
+      data["Data"] = rows;
+      res.json(data);
+    } else {
+      data["Data"] = "No data Found..";
+      res.json(data);
+    }
+  });
+});
+
+
+app.get("/matchOrder", (req, res) => {
+     console.log('req: ', req.query);
+    var order=req.query;
+  const sql = "select * from orders where purchase = ? and price >= ?";
+  var data = {
+    Data: "",
+  };
+  db.query(sql, [order.purchase, order.price], function (err, rows, fields) {
+    if (rows.length != 0) {
+      data["Data"] = rows;
+      res.json(data);
+    } else {
+      data["Data"] = "No data Found..";
+      res.json(data);
+    }
+  });
+});
+
+
 
 app.listen(3001,() => {
     console.log("running on port 3001");
